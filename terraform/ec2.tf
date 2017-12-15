8e1f90abef67b0de1275424392cb6f98eda13421
tidmmar <marktid@marks-imac>
2017-12-15 19:53:28 +0000

    Added security groups

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,4 +8,11 @@ provider "aws" {
 resource "aws_instance" "dev_host1" {
   ami           = "ami-e7d6c983"
   instance_type = "t2.micro"
+  security_groups = [
+        "${aws_security_group.first.id}",
+        "${aws_security_group.second.id}"
+    ]
+    tags {
+        Name = "Development Host"
+    }
 }
