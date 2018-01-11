2553307577d2204f381148d7c77adfaab6403ec3
tidmmar <marktid@marks-imac>
2018-01-11 13:45:41 +0000

    Fixed a bug where multiple tags for a single resource were not being picked up

--- aws/aws_tags.py
+++ aws/aws_tags.py
@@ -9,20 +9,19 @@ import boto3
 
 def list_tags(describe_tags):
     for t in describe_tags['Tags']:
-        if t['ResourceId'] not in resourceid: 
-            seen = False
-            for k in t:
-                if k == 'ResourceId':
-                    continue
-                elif k == 'ResourceType' and seen == False:
-                    resourceid.setdefault(t['ResourceId'],[]).insert(0, k)
-                    resourceid.setdefault(t['ResourceId'],[]).insert(1, t[k])
-                    seen = True
-                elif k == 'ResourceType' and seen == True:
-                    continue
-                else:
-                    resourceid.setdefault(t['ResourceId'],[]).append(k)
-                    resourceid.setdefault(t['ResourceId'],[]).append(t[k])
+        seen = False
+        for k in t:
+            if k == 'ResourceId':
+                continue
+            elif k == 'ResourceType' and seen == False:
+                resourceid.setdefault(t['ResourceId'],[]).insert(0, k)
+                resourceid.setdefault(t['ResourceId'],[]).insert(1, t[k])
+                seen = True
+            elif k == 'ResourceType' and seen == True:
+                continue
+            else:
+                resourceid.setdefault(t['ResourceId'],[]).append(k)
+                resourceid.setdefault(t['ResourceId'],[]).append(t[k])
             
 ec2 = boto3.resource('ec2')
 ec2client = boto3.client('ec2')
