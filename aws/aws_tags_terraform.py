5b9bb9c4183ad1455cb055eefbaffdb23cb9ec5a
tidmmar <marktid@marks-imac>
2018-01-11 13:48:49 +0000

    Fixed a bug where multiple level of tags were being missed on resources

--- aws/aws_tags_terraform.py
+++ aws/aws_tags_terraform.py
@@ -9,21 +9,20 @@
 import boto3
 
 def list_tags(describe_tags):
-    for t in describe_tags['Tags']:
-        if t['ResourceId'] not in resourceid: 
-            seen = False
-            for k in t:
-                if k == 'ResourceId':
-                    continue
-                elif k == 'ResourceType' and seen == False:
-                    resourceid.setdefault(t['ResourceId'],[]).insert(0, k)
-                    resourceid.setdefault(t['ResourceId'],[]).insert(1, t[k])
-                    seen = True
-                elif k == 'ResourceType' and seen == True:
-                    continue
-                else:
-                    resourceid.setdefault(t['ResourceId'],[]).append(k)
-                    resourceid.setdefault(t['ResourceId'],[]).append(t[k])
+    for t in describe_tags['Tags']: 
+        seen = False
+        for k in t:
+            if k == 'ResourceId':
+                continue
+            elif k == 'ResourceType' and seen == False:
+                resourceid.setdefault(t['ResourceId'],[]).insert(0, k)
+                resourceid.setdefault(t['ResourceId'],[]).insert(1, t[k])
+                seen = True
+            elif k == 'ResourceType' and seen == True:
+                continue
+            else:
+                resourceid.setdefault(t['ResourceId'],[]).append(k)
+                resourceid.setdefault(t['ResourceId'],[]).append(t[k])
             
 ec2 = boto3.resource('ec2')
 ec2client = boto3.client('ec2')
